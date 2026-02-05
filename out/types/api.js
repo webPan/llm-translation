1c6c3ad7c69a26f0e39bead4e1bcf06785f13996
qing_321 <qing_321@126.com>
2026-02-05 14:11:21 +0800

    feat:  忽略out 的提交

--- out/types/api.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// API Request/Response types
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=api.js.map
--- out/types/language.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SUPPORTED_LANGUAGES = void 0;
-exports.SUPPORTED_LANGUAGES = [
-    { code: 'zh', name: 'Chinese', localName: '中文' },
-    { code: 'en', name: 'English', localName: 'English' },
-    { code: 'ja', name: 'Japanese', localName: '日本語' },
-];
-//# sourceMappingURL=language.js.map
